dc962e4d66bc20432c6b6d85d0a0d0ac7edca907
Tarik Doganay <99910240+GoodGuyForReal@users.noreply.github.com>
2023-03-07 16:26:59 +0100

    empty project logo file path bug fixed

--- src/components/pages/kanbanBoard/KanbanCompanents/ProjectSettingsCard/ProjectSettings.jsx
+++ src/components/pages/kanbanBoard/KanbanCompanents/ProjectSettingsCard/ProjectSettings.jsx
@@ -22,7 +22,10 @@ const ProjectSettings = ({ docRef, setIsProjectSettingsOpen }) => {
     const navigate = useNavigate()
 
     const handleFileChange = (e) => {
-        if (e.target.files[0]) {
+        if (e.target.files[0].size > 2019066) {
+            alert('Dude this file is waaay to big. Size limit is 2mb')
+            e.target.value = ''
+        } else {
             setFile(e.target.files[0])
         }
     };
@@ -34,34 +37,40 @@ const ProjectSettings = ({ docRef, setIsProjectSettingsOpen }) => {
         }
     }, [projectData])
 
+    const fileName = getFilenameFromUrl(projectData.projectlogo)
+    console.log(fileName);
+
     const handleUpdateProject = async (e) => {
         e.preventDefault()
         let imgUrl;
         if (file) {
             const fileName = getFilenameFromUrl(projectData.projectlogo)
-            const storageRef = ref(storage, `${user.uid}/Projects/ProjecstLogo/${fileName}`);
+            const newFilename = Date.now() + '_' + Math.random().toString(36).substr(2, 9);
+            const optionalFileName = fileName !== '' ? fileName : newFilename
+            const storageRef = ref(storage, `${user.uid}/Projects/ProjecstLogo/${optionalFileName}`);
             setUploading(true)
             imgUrl = await handleFileUpload(storageRef, file, file?.type)
-            setUploading(false)
         }
         await updateProjectSettings(projectName, projectData.projectlogo, imgUrl, user, docRef)
-        console.log('updated');
-        setFile(null)
+        setUploading(false)
+        //  console.log('updated');
     }
 
     const handlerRemoveProject = async () => {
-        if (projectData.projectlogo) {
-            const getFileName = getFilenameFromUrl(projectData.projectlogo)
-            console.log(getFileName);
-            const storageRef = ref(storage, `${user.uid}/Projects/ProjecstLogo/${getFileName}`);
-            await deleteObject(storageRef)
-        }
         let text = "Are you sure about that";
         // eslint-disable-next-line no-restricted-globals
         if (confirm(text) === true) {
             await removeProjectHandler(user, docRef)
             navigate('/dashboard')
             setDocRefId('')
+
+            if (projectData.projectlogo) {
+                const fileName = getFilenameFromUrl(projectData.projectlogo)
+                console.log(fileName);
+                const storageRef = ref(storage, `${user.uid}/Projects/ProjecstLogo/${fileName}`);
+                await deleteObject(storageRef)
+            }
+
         }
     }
 
@@ -91,7 +100,7 @@ const ProjectSettings = ({ docRef, setIsProjectSettingsOpen }) => {
                     </div>
 
                     <div className="flex flex-col items-center w-full gap-5">
-                        {uploading && <h2 className='py-3 mb-3 px-6 border rounded-lg bg-slate-100'>Image uploading...</h2>}
+                        {uploading && <h2 className='py-3 px-6 mb-3 w-full border rounded-lg bg-slate-100'>Image uploading...</h2>}
                         <button onClick={handleUpdateProject} className="bg-blue-600 hover:bg-blue-700 text-white font-medium py-2 w-full rounded focus:outline-none focus:shadow-outline" type="button">
                             Update
                         </button>
