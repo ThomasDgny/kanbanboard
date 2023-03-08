fd6c8d43afa8aa1a05c5061772ccd8869eb7d7ec
Tarik Doganay <99910240+GoodGuyForReal@users.noreply.github.com>
2023-03-08 01:24:20 +0100

    feature section updatte

--- src/components/pages/main/mainSections/FeaturesSection.jsx
+++ src/components/pages/main/mainSections/FeaturesSection.jsx
@@ -27,13 +27,13 @@ const FeaturesSection = () => {
                     <div className="bg-gradient-to-tr h-full from-slate-200 to-slate-100 col-span-12 md:col-span-8 p-1 rounded-2xl">
                         <div className='relative bg-gradient-to-tr h-full from-[#f2f2f2] via-[#a9d2f8] to-[#ffffff] rounded-2xl w-ful overflow-hidden '>
                             <div className='absolute z-[5] h-full w-full flex flex-col gap-5'>
-                                <div className='py-10 px-24'>
-                                    <h1 className='text-[34px] text-slate-800 font-bold select-none'>Everything is possible</h1>
-                                    <p className='text-slate-500 text-[18px] font-medium max-w-2xl select-none'>Create amazing text with our new text editor that loads faster and is more responsive. Our new text editor is designed to save time in your daily life.</p>
+                                <div className='py-10 px-8 md:px-24'>
+                                    <h1 className='text-[34px] text-slate-800 font-bold'>Everything is possible</h1>
+                                    <p className='text-slate-500 text-[18px] font-medium max-w-2xl'>Create amazing text with our new text editor that loads faster and is more responsive. Our new text editor is designed to save time in your daily life.</p>
                                 </div>
 
                                 <div className='overflow-hidden w-full h-96 scale-125 flex justify-center pt-[3vh]'>
-                                    <div className='h-full w-[65%] TextArea bg-white rounded-lg p-5 overflow-hidden drop-shadow-md'>
+                                    <div className='h-full w-[75%] md:w-[65%] TextArea bg-white rounded-lg p-5 overflow-hidden drop-shadow-md'>
                                         <TextEditor
                                             content={''}
                                             setContent={''}
@@ -59,8 +59,8 @@ const FeaturesSection = () => {
                     <div className="col-span-12 md:col-span-4 bg-gradient-to-tr from-slate-200 to-slate-100 p-1 rounded-2xl">
                         <div className=' bg-gradient-to-r h-full from-[#0d113b] to-[#000212] rounded-2xl w-ful overflow-hidden flex flex-col justify-between gap-5'>
                             <div className='p-10'>
-                                <h1 className='text-[32px] text-white font-bold select-none'>Edit Your Task</h1>
-                                <p className='text-slate-300 text-[18px] font-medium max-w-xl select-none'>Track your tasks, update them and even delete them if you don't need them any more.</p>
+                                <h1 className='text-[32px] text-white font-bold'>Edit Your Task</h1>
+                                <p className='text-slate-300 text-[18px] font-medium max-w-xl'>Track your tasks, update them and even delete them if you don't need them any more.</p>
                             </div>
 
                             <div className='w-full h-full flex justify-center'>
@@ -75,8 +75,8 @@ const FeaturesSection = () => {
                 <div className=" col-span-1 bg-gradient-to-tr  from-slate-200 to-slate-100 sm:col-span-8 p-1 rounded-2xl">
                     <div className='relative bg-gradient-to-tr h-full from-[#f2f2f2] via-[#cad3ff] to-[#ffffff] rounded-2xl w-ful overflow-hidden flex flex-col justify-between gap-5 md:gap-20'>
                         <div className='p-10'>
-                            <h1 className='text-[32px] text-slate-800 font-bold'>Create Task</h1>
-                            <p className='text-slate-600 text-[18px] font-medium max-w-xl'>Fly through your tasks with rapid-fire keyboard shortcuts for everything. Literally everything.</p>
+                            <h1 className='text-[32px] text-white font-bold'>Follow Your Progress</h1>
+                            <p className='text-slate-300 text-[18px] font-medium max-w-2xl'>buyuk olan kisimda genel progress takip eden card olucak bir de proje karti ikisini de figmada kombine edip svg seklinde buraya koy diger kucuk de ise yazi olacuk sadece.</p>
                         </div>
 
                         <div className='overflow-hidden w-full h-96 scale-100 md:scale-125 md:flex md:justify-center'>
