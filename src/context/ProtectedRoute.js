c80afb58ccf315194acc65f6ee0d9e2f2c99fc1a
Tarik Doganay <99910240+GoodGuyForReal@users.noreply.github.com>
2023-03-12 09:10:03 +0100

    protected route update

--- src/context/ProtectedRoute.js
+++ src/context/ProtectedRoute.js
@@ -1,26 +1,13 @@
 import { Navigate } from "react-router-dom";
 import { UserAuth } from "./UserAuth";
 
-// const ProtectedRoute = async ({ child }) => {
-//     const user = UserAuth();
-//     const waitUserData = (user) => {
-//         if (user) {
-//             return child
-//         } else {
-//             return <Navigate to={'/signin'} />
-//         }
-//     }
-//     await waitUserData(user)
-
-// };
-
-// export default ProtectedRoute;
-
 const ProtectedRoute = ({ child }) => {
     const user = UserAuth();
-    return (
-        user ? child : <Navigate to={'/signin'} />
-    )
-};
+    if (user.uid) {
+        return child
+    } else {
+        return <Navigate to={'/signin'} />
+    }
+}
 
 export default ProtectedRoute;
